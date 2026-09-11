8eddbd8f0477a98f1e1072c2d3870943e0848de7
sweetpresents31-svg <sweetpresents31@gmail.com>
2026-09-11 08:54:05 -0500

    Bundle all storefront modules to fix blank production page

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./original-categories.css";
+import "./sweet-blooms-branding.css";
+import "./luxury-room.css";
+import "./home-product-carousel.css";
 import "./original-categories.js";
+import "./sweet-blooms-branding.js";
+import "./luxury-room.js";
+import "./home-product-carousel.js";
+import "./store-location.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
